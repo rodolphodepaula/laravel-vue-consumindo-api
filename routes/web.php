6569c32b15671b1005da7d76bae3c1fd40e2913c
rodolphosp <rodolphosp@gmail.com>
2023-11-16 18:05:43 -0300

    Comentando dialog

--- routes/web.php
+++ routes/web.php
@@ -45,7 +45,7 @@ Route::group([
 ], function() {
     Route::get('/', [BeerController::class, 'index'])->name('beers');
 
-    Route::get('/export', [BeerController::class, 'export'])->name('beers.export');
+    Route::post('/export', [BeerController::class, 'export'])->name('beers.export');
 
     Route::resource("/reports", ExportController::class)
         ->only(["index", "show", "destroy"]);
